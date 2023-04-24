cdafba04b49c53cc24fb91c455ebd3b446ebe2dc
phsu40 <phsu40@gatech.edu>
2023-04-24 16:20:10 -0700

    Create first get mapping to get the Json data of all movies

--- src/main/java/dev/pohsienhsu/movies/service/MovieService.java
+++ src/main/java/dev/pohsienhsu/movies/service/MovieService.java
@@ -1,13 +1,20 @@
 package dev.pohsienhsu.movies.service;
 
+import dev.pohsienhsu.movies.entity.Movie;
 import dev.pohsienhsu.movies.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovieService {
 
     @Autowired
     private MovieRepository movieRepository;
+
+    public List<Movie> findAllMovies() {
+        return this.movieRepository.findAll();
+    }
 
 }
