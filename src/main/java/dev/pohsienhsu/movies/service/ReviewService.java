9799d26545e84a90c11cc0c3fce5945e6c3962c7
phsu40 <phsu40@gatech.edu>
2023-04-24 22:44:54 -0700

    Create full CRUD features for Review entity

---
+++ src/main/java/dev/pohsienhsu/movies/service/ReviewService.java
@@ -0,0 +1,39 @@
+package dev.pohsienhsu.movies.service;
+
+import dev.pohsienhsu.movies.entity.Movie;
+import dev.pohsienhsu.movies.entity.Review;
+import dev.pohsienhsu.movies.repository.ReviewRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public List<Review> allReviews() {
+        return this.reviewRepository.findAll();
+    }
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
