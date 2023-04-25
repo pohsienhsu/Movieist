097a894021dbf8e3a1be6a4a6724fbcf0c0357c4
phsu40 <phsu40@gatech.edu>
2023-04-24 20:18:22 -0700

    Create endpoints to get Movie by id

--- src/main/java/dev/pohsienhsu/movies/controller/MovieController.java
+++ src/main/java/dev/pohsienhsu/movies/controller/MovieController.java
@@ -2,14 +2,17 @@ package dev.pohsienhsu.movies.controller;
 
 import dev.pohsienhsu.movies.entity.Movie;
 import dev.pohsienhsu.movies.service.MovieService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -19,8 +22,13 @@ public class MovieController {
     private MovieService movieService;
 
     @GetMapping
-    public ResponseEntity<List<Movie>> findAllMovies() {
-        return new ResponseEntity<List<Movie>>(this.movieService.findAllMovies(), HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(this.movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable("id") ObjectId id) {
+        return new ResponseEntity<Movie>(this.movieService.MovieById(id), HttpStatus.OK);
     }
 
 
