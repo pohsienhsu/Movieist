6e7cfe3b0123244325ffe35b8e0bf49c6951f956
phsu40 <phsu40@gatech.edu>
2023-05-07 16:50:52 -0700

    Integrate cross origin attributes for React front-end

--- src/main/java/dev/pohsienhsu/movies/controller/MovieController.java
+++ src/main/java/dev/pohsienhsu/movies/controller/MovieController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/movies")
-@CrossOrigin(origins="http://localhost:3000")
 public class MovieController {
 
     @Autowired
--- src/main/java/dev/pohsienhsu/movies/controller/ReviewController.java
+++ src/main/java/dev/pohsienhsu/movies/controller/ReviewController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
